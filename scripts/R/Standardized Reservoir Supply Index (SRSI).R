82c7f88a77dbb33efe605e90e6b2c5246fb82c25
maxmarumbwa <maxmarumbwa@gmail.com>
2024-11-27 15:21:02 +0200

    Multiple graphs on 1 plot

--- scripts/R/Standardized Reservoir Supply Index (SRSI).R
+++ scripts/R/Standardized Reservoir Supply Index (SRSI).R
@@ -35,12 +35,17 @@ SRSI<- monthly_reservoir_data %>%
   )
 SRSI
 
-
 # # Plot SRSI for 1 dam
-# gaborone_data <- SRSI %>% filter(Dam == "Gaborone_dam")
+dam_name <- "Gaborone_dam"
 # 
+# save the SDI
+#write.csv(SRSI, paste0("output/Standardised reservoir supply index/csv/", dam_name, ".csv"), row.names = FALSE)
+
+names(infile)
+
+dam_name <- SRSI %>% filter(Dam == "Gaborone_dam")
 # # Plot SRSI over time for Gaborone_dam
-# ggplot(gaborone_data, aes(x = year_month, y = SRSI)) +
+# ggplot(dam_name, aes(x = year_month, y = SRSI)) +
 #   geom_line(color = "blue", size = 1) +
 #   #geom_point(color = "red", size = 2) +
 #   labs(
@@ -64,8 +69,8 @@ SRSI_levels <- data.frame(
   Threshold = c(-2, -1.5, -1, 0, 1, 1.5, 2)
 )
 
-# Plot the bar graph
-ggplot(gaborone_data, aes(x = year_month, y = SRSI)) +
+# Plot SRSI for all dams on one page using facets
+ggplot(SRSI, aes(x = year_month, y = SRSI)) +
   geom_bar(stat = "identity", fill = "skyblue", alpha = 0.8) +  # Bar graph
   geom_hline(data = SRSI_levels, aes(yintercept = Threshold, color = Category), 
              linetype = "dashed", size = 0.7) +  # SRSI Classification Lines
@@ -73,8 +78,9 @@ ggplot(gaborone_data, aes(x = year_month, y = SRSI)) +
     values = c("red", "blue", "yellow", "green", "lightblue", "brown", "darkblue"),
     guide = guide_legend(title = "SRSI Classification")
   ) +
+  facet_wrap(~ Dam, scales = "free_y") +  # Create subplots for each dam
   labs(
-    title = "SRSI for Gaborone Dam ",
+    title = "Standardised Reservoir Supply Index (SRSI)",
     x = "Date",
     y = "SRSI"
   ) +
@@ -82,6 +88,30 @@ ggplot(gaborone_data, aes(x = year_month, y = SRSI)) +
   theme(
     plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
     axis.text = element_text(size = 8),
-    axis.title = element_text(size = 9),
-    legend.position = "right"
+    axis.title = element_text(size = 10),
+    legend.position = "bottom"
   )
+
+# # Plot the bar graph 1 dam
+# ggplot(dam_name, aes(x = year_month, y = SRSI)) +
+#   geom_bar(stat = "identity", fill = "skyblue", alpha = 0.8) +  # Bar graph
+#   geom_hline(data = SRSI_levels, aes(yintercept = Threshold, color = Category), 
+#              linetype = "dashed", size = 0.7) +  # SRSI Classification Lines
+#   scale_color_manual(
+#     values = c("red", "blue", "yellow", "green", "lightblue", "brown", "darkblue"),
+#     guide = guide_legend(title = "SRSI Classification")
+#   ) +
+#   labs(
+#     title = "SRSI for Gaborone Dam ",
+#     x = "Date",
+#     y = "SRSI"
+#   ) +
+#   theme_minimal() +
+#   theme(
+#     plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
+#     axis.text = element_text(size = 12),
+#     axis.title = element_text(size = 13),
+#     legend.position = "right"
+#   )
+
+
